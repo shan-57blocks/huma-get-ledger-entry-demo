0f7731d6705d686ec53f484f6e4c668140db4045
shan <shan@57blocks.com>
2024-06-07 10:53:53 +0800

    extend TTL

--- src/contract/lib.rs
+++ src/contract/lib.rs
@@ -11,20 +11,20 @@ impl IncrementContract {
     /// Increment increments an internal counter, and returns the value.
     pub fn increment(env: Env) -> u32 {
         // Get the current count.
-        let mut count: u32 = env.storage().instance().get(&COUNTER).unwrap_or(0); // If no value set, assume 0.
+        let mut count: u32 = env.storage().persistent().get(&COUNTER).unwrap_or(0); // If no value set, assume 0.
         log!(&env, "count: {}", count);
 
         // Increment the count.
         count += 1;
 
         // Save the count.
-        env.storage().instance().set(&COUNTER, &count);
+        env.storage().persistent().set(&COUNTER, &count);
 
         // The contract instance will be bumped to have a lifetime of at least 100 ledgers if the current expiration lifetime at most 50.
         // If the lifetime is already more than 100 ledgers, this is a no-op. Otherwise,
         // the lifetime is extended to 100 ledgers. This lifetime bump includes the contract
         // instance itself and all entries in storage().instance(), i.e, COUNTER.
-        env.storage().instance().extend_ttl(50, 100);
+        // env.storage().instance().extend_ttl(50, 100);
 
         // Return the count to the caller.
         count
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,88 @@
-import { Address, SorobanRpc, xdr } from '@stellar/stellar-sdk';
+import {
+  Address,
+  BASE_FEE,
+  Keypair,
+  Networks,
+  Operation,
+  SorobanDataBuilder,
+  SorobanRpc,
+  TransactionBuilder,
+  xdr
+} from '@stellar/stellar-sdk';
+
+const getLedgerKeySymbol = (contractId: string, symbolText: string) => {
+  const ledgerKey = xdr.LedgerKey.contractData(
+    new xdr.LedgerKeyContractData({
+      contract: new Address(contractId).toScAddress(),
+      key: xdr.ScVal.scvSymbol(symbolText),
+      durability: xdr.ContractDataDurability.persistent()
+    })
+  );
+  return ledgerKey;
+};
+
+const extendTTL = async (
+  ledgerKeys: xdr.LedgerKey,
+  server: SorobanRpc.Server
+) => {
+  const signer = Keypair.fromSecret(
+    'SCS43KPAQZ2J22DYMAFJQVFTPWZQ6RBYL332PEBCPBJ6ZHAF3PMR2VLC'
+  );
+  const account = await server.getAccount(signer.publicKey());
+  const tx = new TransactionBuilder(account, { fee: BASE_FEE })
+    .setNetworkPassphrase(Networks.TESTNET)
+    .setSorobanData(new SorobanDataBuilder().setReadWrite([ledgerKeys]).build())
+    .addOperation(
+      Operation.extendFootprintTtl({
+        extendTo: 3110400 - 1
+      })
+    )
+    .setTimeout(30)
+    .build();
+
+  const preparedTransaction = await server.prepareTransaction(tx);
+  preparedTransaction.sign(signer);
+
+  const sendResponse = await server.sendTransaction(preparedTransaction);
+  if (sendResponse.status === 'PENDING') {
+    let getResponse = await server.getTransaction(sendResponse.hash);
+    while (getResponse.status === 'NOT_FOUND') {
+      console.log('Extend TTL Pending...');
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      getResponse = await server.getTransaction(sendResponse.hash);
+    }
+
+    if (getResponse.status === 'SUCCESS') {
+      if (!getResponse.resultMetaXdr) {
+        throw 'Empty resultMetaXDR in getTransaction response';
+      }
+      const transactionMeta = getResponse.resultMetaXdr;
+      console.log('success');
+      console.log(transactionMeta.v3().sorobanMeta().returnValue());
+    } else {
+      throw getResponse.resultXdr;
+    }
+  } else {
+    throw sendResponse.errorResult;
+  }
+};
 
 (async () => {
-  const getLedgerKeySymbol = (contractId: string, symbolText: string) => {
-    const ledgerKey = xdr.LedgerKey.contractData(
-      new xdr.LedgerKeyContractData({
-        contract: new Address(contractId).toScAddress(),
-        key: xdr.ScVal.scvSymbol(symbolText),
-        durability: xdr.ContractDataDurability.persistent()
-      })
-    );
-    return ledgerKey;
-  };
-
   const incrementContract =
-    'CCVRLKWWSYYFAEOCMLH5BT5B3SWCJSO6SMB5S3LOZ5BMKE6D6PTCXJRL';
+    'CDEFDSU2W56KZW7K226JDQGPZIGEN2NZIDCRR4T34DKNMALEUWCIRJBL';
   const COUNTER_SYMBOL = 'COUNTER';
-  const keys = getLedgerKeySymbol(incrementContract, COUNTER_SYMBOL);
-
+  const ledgerKeys = getLedgerKeySymbol(incrementContract, COUNTER_SYMBOL);
   const server = new SorobanRpc.Server('https://soroban-testnet.stellar.org');
-  const entries = await server.getLedgerEntries(keys);
-  console.log(entries);
+
+  const entriesBeforeExtend = await server.getLedgerEntries(ledgerKeys);
+  console.log('COUNTER entries before extend TTL: ', entriesBeforeExtend);
+  console.log('****************');
+  console.log('****************');
+
+  await extendTTL(ledgerKeys, server);
+
+  const entriesAfterExtend = await server.getLedgerEntries(ledgerKeys);
+  console.log('****************');
+  console.log('****************');
+  console.log('COUNTER entries after extend TTL: ', entriesAfterExtend);
 })();
